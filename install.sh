728744e70828b4e11312a088d3b4639461f0b298
Bryan Atkinson <bryan.atkinson@gmail.com>
2014-07-23 10:39:38 -0400

    Ensuring powerline font is installed for vim

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Copy powerline font
+if [ ! -d "~/.fonts" ]; then
+  mkdir ~/.fonts
+fi
+
+if [ ! -e "~/.fonts/Droid\ Sans\ Mono\ for\ Powerline.otf" ]; then
+   wget -P ~/.fonts/ https://github.com/Lokaltog/powerline-fonts/raw/master/DroidSansMono/Droid%20Sans%20Mono%20for%20Powerline.otf
+   fc-cache -vf ~/.fonts
+fi
+
 # install pathogen
 wget -P ~/.vim/autoload https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim 
 
